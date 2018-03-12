0ec3208bb430ee4487cdd45fc6cb2559c43d3dba
rajesh.n <rajesh.n@IZDT-211.networksrv.com>
2018-03-12 09:14:19 +0530

    Signed-off-by: rajesh.n <rajesh.n@IZDT-211.networksrv.com>

--- src/test/java/utility/ExtentReporterNG.java
+++ src/test/java/utility/ExtentReporterNG.java
@@ -14,6 +14,57 @@ public  class ExtentReporterNG{
 		return report;
 	
 	}
+	
+	/* public static ExtentHtmlReporter htmlReporter;
+	    public static ExtentReports extent;
+	    public static ExtentTest test;
+	     
+	    @BeforeSuite
+	    public void setUp()
+	    {
+	        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"/test-output/MyOwnReport.html");
+	        extent = new ExtentReports();
+	        extent.attachReporter(htmlReporter);
+	         
+	        extent.setSystemInfo("OS", "Mac Sierra");
+	        extent.setSystemInfo("Host Name", "Krishna");
+	        extent.setSystemInfo("Environment", "QA");
+	        extent.setSystemInfo("User Name", "Krishna Sakinala");
+	         
+	        htmlReporter.config().setChartVisibilityOnOpen(true);
+	        htmlReporter.config().setDocumentTitle("AutomationTesting.in Demo Report");
+	        htmlReporter.config().setReportName("My Own Report");
+	        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+	        htmlReporter.config().setTheme(Theme.DARK);
+	    }
+	     
+	    @AfterMethod
+	    public void getResult(ITestResult result)
+	    {
+	        if(result.getStatus() == ITestResult.FAILURE)
+	        {
+	            test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" Test case FAILED due to below issues:", ExtentColor.RED));
+	            test.fail(result.getThrowable());
+	        }
+	        else if(result.getStatus() == ITestResult.SUCCESS)
+	        {
+	            test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" Test Case PASSED", ExtentColor.GREEN));
+	        }
+	        else
+	        {
+	            test.log(Status.SKIP, MarkupHelper.createLabel(result.getName()+" Test Case SKIPPED", ExtentColor.ORANGE));
+	            test.skip(result.getThrowable());
+	        }
+	    }
+	     
+	    @AfterSuite
+	    public void tearDown()
+	    {
+	        extent.flush();
+	    }
+	}
+	*/
+
 }
     
 
